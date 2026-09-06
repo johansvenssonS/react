882817e563108e4e426f8ccee600d66043a0cce3
Johan Svensson <johan.svensson0608@gmail.com>
2026-09-06 23:07:57 +0200

    demo reversetext

--- demo/src/SquareSection.jsx
+++ demo/src/SquareSection.jsx
@@ -1,6 +1,6 @@
-function Square({color, size}) {
-    return (
-        <div style={{backgroundColor: color, width: size, height: size,}}></div>
-    )
+function Square({ color, size }) {
+  return (
+    <div style={{ backgroundColor: color, width: size, height: size }}></div>
+  );
 }
-export default Square
+export default Square;
